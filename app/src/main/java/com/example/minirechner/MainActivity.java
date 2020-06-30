42f5eab185a189ecb90125020854053a08ee8831
cm353 <christian.meyenburg@gmail.com>
2020-06-30 11:07:22 +0100

    UI changed: TextView for the result field Fontsize changed

--- app/src/main/java/com/example/minirechner/MainActivity.java
+++ app/src/main/java/com/example/minirechner/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     EditText field2;
     TextView field3;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
