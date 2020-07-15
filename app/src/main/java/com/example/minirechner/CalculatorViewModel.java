c81eb424d334384f665adcf14277e8909050882a
cm353 <christian.meyenburg@gmail.com>
2020-07-15 14:18:04 +0100

    ViewModel changed

--- app/src/main/java/com/example/minirechner/CalculatorViewModel.java
+++ app/src/main/java/com/example/minirechner/CalculatorViewModel.java
@@ -10,14 +10,6 @@ public class CalculatorViewModel extends ViewModel {
     private Double memory = null;
     private int index = 0;
 
-    public Double getNumber1() {
-        return number1;
-    }
-
-    public Double getNumber2() {
-        return number2;
-    }
-
     public Double getResult() {
         return result;
     }
@@ -26,20 +18,28 @@ public class CalculatorViewModel extends ViewModel {
         return memory;
     }
 
-    public int getIndex() {
-        return index;
+    public void setMemory(Double memory) {
+        this.memory = memory;
+    }
+
+    public Double getNumber1() {
+        return number1;
     }
 
     public void setNumber1(Double number1) {
         this.number1 = number1;
     }
 
+    public Double getNumber2() {
+        return number2;
+    }
+
     public void setNumber2(Double number2) {
         this.number2 = number2;
     }
 
-    public void setMemory(Double memory) {
-        this.memory = memory;
+    public int getIndex() {
+        return index;
     }
 
     private void count(){
